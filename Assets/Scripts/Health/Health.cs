ad5501d2e9bda475a631dd882ab10d9f1d2e4298
Kaustubh0912 <kaustubharun2003@gmail.com>
2024-05-05 13:01:01 +0530

    Saw Trap

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header("Health")]
     [SerializeField] private float startingHealth;
     public float currenthealth {  get; private set; }
     private Animator anim;
     private bool dead;
+
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRenderer;
     private void Awake()
     {
         currenthealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
     public void TakeDamage(float _damage)
     {
@@ -20,6 +27,7 @@ public class Health : MonoBehaviour
         if(currenthealth > 0 ) 
         {
             anim.SetTrigger("Hurt");
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -31,6 +39,23 @@ public class Health : MonoBehaviour
             }
         }
 
+    }
+    public void AddHealth(float _health)
+    {
+        currenthealth = Mathf.Clamp(currenthealth + _health, 0, startingHealth);
+    }
+
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(8,9,true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRenderer.color = new Color(1,0,0,0.5f); 
+            yield return new WaitForSeconds(iFramesDuration/(numberOfFlashes*2)); 
+            spriteRenderer.color=Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(8, 9, false);
     }
     
 }
